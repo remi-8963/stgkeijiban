33422d62a724648bfa71dd29beabce3e346c645b
remi-8963 <motoki1701@gmail.com>
2021-05-17 16:12:01 +0900

    データベースの接続情報を編集できるようにした

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,8 @@
 <?php
 
-$db =  mysqli_connect('localhost','root','','test_db');
+require_once('./environment.php');
+
+$db =  mysqli_connect('localhost',$DATABASE_USERNAME,$DATABASE_PASSWORD,$DATABASE_TABLE);
 
 mysqli_set_charset($db,'utf8');
 
---
+++ environment_example.php
@@ -0,0 +1,5 @@
+<?php
+
+$DATABASE_USERNAME = 'root';
+$DATABASE_PASSWORD = '';
+$DATABASE_TABLE = 'test_db';
