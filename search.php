21e613c4c4f428f654de8a3035f5ab5dab7f96f3
michinosuke <michiya.318.3738@gmail.com>
2021-06-09 02:13:09 +0900

    検索結果の見た目の変更

--- search.php
+++ search.php
@@ -1,3 +1,15 @@
+<?php
+
+require('dbconnect.php');
+
+// $_GET['name']が入ってなかったら処理を終了
+$search_name = $_GET['name'] ?? null or die();
+
+$sql = "SELECT id, name FROM users WHERE name LIKE '%$search_name%' ORDER BY name DESC";
+
+$match_users = mysqli_query($db, $sql) or die('SQLのエラー');
+
+?>
 <!DOCTYPE html>
 
 <html>
@@ -9,29 +21,15 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
     </head>
     <body>
-        <?php
-
-        require('dbconnect.php');
-
-        // /search.php?name=れみ
-        // れみい
-
-        // /search.php
-        // 何も表示されない
-
-        $search_name = $_GET['name'] ?? null; // $_GET['name']が入ってたら$_GET['name']、入ってなかったらnull
-
-        if ($search_name === null) die();
-
-        $sql = "SELECT id, name FROM users WHERE name LIKE '%$search_name%' ORDER BY name DESC";
-
-        $match_users = mysqli_query($db, $sql) or die('SQLのエラー');
-
-        while($match_user = mysqli_fetch_assoc($match_users)): ?>
-
-        <p><?=$match_user['id']?>: <a href="user.php?id=<?=$match_user['id']?>"><?=$match_user['name']?></a></p>
-
+        <h1>"<?=$search_name?>"の検索結果(<?=mysqli_num_rows($match_users)?>件)</h1>
+        <table>
+        <tr>
+            <th>ID</th><th>ユーザ名</th>
+        </tr>
+        <?php while($match_user = mysqli_fetch_assoc($match_users)): ?>
+            <td><?=$match_user['id']?></td>
+            <td><a href="user.php?id=<?=$match_user['id']?>"><?=$match_user['name']?></a></td>
         <?php endwhile; ?>
-
+        </table>
     </body>
 </html>
