2c2312fafdf43e34d51d4ec5e069be41435c1c85
remi-8963 <motoki1701@gmail.com>
2021-07-06 18:51:15 +0900

    imgの表示変更

--- img.php
+++ img.php
@@ -6,7 +6,7 @@ $sql = sprintf('SELECT play_style, name, sex, active_time, comment FROM users WH
     s($_GET['id'])
 );
 
-$sql_users_games = sprintf('SELECT  title, kill_rate, ranking FROM users JOIN users_games ON users.id = users_games.user_id JOIN games ON users_games.game_id = games.id WHERE users.id = %d',
+$sql_users_games = sprintf('SELECT title FROM users JOIN users_games ON users.id = users_games.user_id JOIN games ON users_games.game_id = games.id WHERE users.id = %d',
     s($_GET['id'])
 );
 $result = mysqli_query($db,$sql) or die (mysqli_error($db));
@@ -18,19 +18,34 @@ if (!$row) {
     exit();
 }
 
-extract($row);
-
-$result_users_games = mysqli_query($db,$sql_users_games) or die(mysqli_error($db));
-$row_games = mysqli_fetch_assoc($result_users_games);
-
-
-
-
 $image = imagecreatefromjpeg('./template1.jpg');
 $icon_image = imagecreatefrompng('./icon_image.png');
 
-$text_title = mb_convert_encoding($row_games['title'], "UTF-8", "auto");
-imagettftext($image, 30, 0, 650, 150, $textcolor, './rounded-mplus-1c-bold.ttf', $text_title);
+extract($row);
+
+$x = 650;
+$y = 130;
+
+$result_users_games = mysqli_query($db,$sql_users_games) or die(mysqli_error($db));
+
+// while($row_games = mysqli_fetch_assoc($result_users_games)){
+//     $text_title = mb_convert_encoding($row_games['title'], "UTF-8", "auto");
+//     imagettftext($image, 30, 0, $x, $y, $textcolor, './rounded-mplus-1c-bold.ttf', $text_title);
+//     $y += 50;
+// }
+for($a=0;$a<3;$a++){
+    for($i=0;$i<3;$i++){
+        $row_games = mysqli_fetch_assoc($result_users_games);
+        for($j=0;$j<1;$j++){
+            $text_title = mb_convert_encoding($row_games['title'], "UTF-8", "auto");
+            imagettftext($image, 30, 0, $x, $y, $textcolor, './rounded-mplus-1c-bold.ttf', $text_title);
+            $y += 50;
+        }
+    }
+    $y = 130;
+    $x += 120;
+}
+
 
 $text_name = mb_convert_encoding($name, "UTF-8", "auto");
 imagettftext($image, 20, 0, 270, 345, $textcolor, './rounded-mplus-1c-bold.ttf', $text_name);
